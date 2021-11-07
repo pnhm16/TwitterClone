621fbb3c919b5a4bc7066df0ac95f7c5b79fda3d
Phi Nguyen Hai Minh <minhpnh.bi10-120@st.usth.edu.vn>
2021-11-07 22:33:44 +0700

    Part 1 complete

--- app/src/main/java/vn/edu/usth/twitterclone/MainActivity.java
+++ app/src/main/java/vn/edu/usth/twitterclone/MainActivity.java
@@ -2,13 +2,33 @@ package vn.edu.usth.twitterclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buSignin, buSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buSignin = findViewById(R.id.buSignIn);
+        buSignup = findViewById(R.id.buSignUpAct);
+        buSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        buSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/vn/edu/usth/twitterclone/SignUpActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.usth.twitterclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
